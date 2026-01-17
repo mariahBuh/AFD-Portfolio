d642e02948ed6089e3f92b2dbdf80d3cc9e4b88d
mariahBuh <159011523+mariahBuh@users.noreply.github.com>
2026-01-17 17:08:50 +0100

    removed storybook

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -1,19 +1,17 @@
 /// <reference types="vitest/config" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-const dirname =
-  typeof __dirname !== "undefined"
-    ? __dirname
-    : path.dirname(fileURLToPath(import.meta.url));
-
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
+
   build: {
     outDir: "dist",
+  },
+
+  test: {
+    environment: "jsdom",
+    globals: true,
   },
 });
